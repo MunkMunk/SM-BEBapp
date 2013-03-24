21107385410ca5eb12c07501d2eaa384e6cac50c
Kellen Zelle <animemunk13@gmail.com>
2013-03-24 16:16:23 -0600

    auto response working

---
+++ sm-bebapp/src/com/example/sm_bebapp/sendSMSActivity.java
@@ -0,0 +1,39 @@
+package com.example.sm_bebapp;
+
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+
+
+public class sendSMSActivity extends Activity {
+
+	String phoneNumber, message;
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        phoneNumber = getIntent().getExtras().getString("phoneNumber");
+        message = getIntent().getExtras().getString("message");
+        
+        sendSMS(phoneNumber,message);
+        
+        
+    }
+    
+    //---sends an SMS message to another device---
+    private void sendSMS(String phoneNumber, String message)
+    {        
+        PendingIntent pi = PendingIntent.getActivity(this, 0,
+            new Intent(this, MainActivity.class), 0);                
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, pi, null);        
+    } 
+	
+}
